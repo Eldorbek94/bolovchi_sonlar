293f5948504545f719a20b4da4979304b78f883a
Eldorbek94 <eldorbtc@gmail.com>
2024-11-01 15:11:20 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Bo'luvchisiz sonlar:
+Console.WriteLine("Bo'luvchilar soni! \nInput: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Output = ");
+bool first=true;
+for (int i=2; i<=10; i++){
+    if(n%i==0){
+        if(!first){
+            Console.Write(",");
+        }
+        Console.Write(i);
+        first =false;
+    }
+
+}
